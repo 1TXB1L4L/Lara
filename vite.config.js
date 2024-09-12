c84a0e24daf2442ea19758734bd17926ce246941
mbilal2913 <mbilal2913@gmail.com>
2024-09-12 14:53:54 +0000

    Prepare for Heroku deployment

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,4 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    build: {
-        outDir: 'public/build', // Ensure this matches where Laravel expects build files
-    },
 });
