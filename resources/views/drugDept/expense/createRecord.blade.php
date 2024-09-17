1bcc7fe07b8d5e9b513115a49f26701e4d9939cb
mbilal2913 <mbilal2913@gmail.com>
2024-09-17 10:54:25 +0000

    change message from status to sucess and info

--- resources/views/drugDept/expense/createRecord.blade.php
+++ resources/views/drugDept/expense/createRecord.blade.php
@@ -39,7 +39,7 @@
             <span class="block sm:inline">{{ session('error') }}</span>
         </div>
         @endif
-        @if(session('status'))
+        @if(session('success'))
         <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded-lg mb-4" role="alert">
             <strong class="font-bold">Success!</strong>
             <span class="block sm:inline">{{ session('status') }}</span>
