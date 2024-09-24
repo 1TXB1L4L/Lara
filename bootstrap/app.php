1ed86d94807583e00f7c74dfa2350109a34fcf9f
mbilal2913 <mbilal2913@gmail.com>
2024-09-24 06:49:07 +0000

    nothing

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,14 +11,16 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__ . '/../routes/api.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
+        then: function () {
+            Route::namespace('Drug')->prefix('drug')->name('drug.')->group(base_path('routes/drug.php'));
+        },
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
         $middleware->alias([
-            'role' => \App\Http\Middleware\RoleMiddleware::class,
+            'role' => RoleMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
-
